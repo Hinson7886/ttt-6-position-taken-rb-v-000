98fba9d521065710ac0b566bb0404a44c0e6f450
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-12 16:24:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || "O"
    return true
-  else board[index] == " " || "" || nil
+ else #board[index] == " " || "" || nil
    return false
   end
 end
